26df5635823f22e7e8586d8585b8f280e98dc2a8
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-15 14:45:12 +0200

    Commit

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -3,4 +3,18 @@
     2. confronta i 2 valori per determinare il vincitore
 */
 
-Math.floor(Math.random())
+let player = Math.floor(Math.random()*6)+1;
+    console.log(player)
+    
+
+let computer = Math.floor(Math.random()*6)+1;
+    console.log(computer)  
+
+if (player < computer){
+    document.getElementById('outcome').innerHTML = 'Peccato! Hai perso';
+    console.log('outcome')
+} else if (player > computer){
+        document.getElementById('outcome').innerHTML = 'Evviva! Hai vinto';
+    } else{
+        document.getElementById('outcome').innerHTML = 'Pareggio!';
+}
