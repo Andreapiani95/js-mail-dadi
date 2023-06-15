5bca579a052a305f5bb3a9223d3a05625e388844
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-15 14:26:22 +0200

    Inizializzazione Esercizio

---
+++ Dadi/js/script.js
@@ -0,0 +1,6 @@
+/*
+    1. randomizza valore di 2 tiri di dado
+    2. confronta i 2 valori per determinare il vincitore
+*/
+
+Math.floor(Math.random())
---
+++ Mail/js/script.js
@@ -0,0 +1,5 @@
+/*
+    1. Prelevare value da un form per ottenere la mail
+    2. Constrollare che sia presente in una lista
+    3. Se sì/Se no
+*/
