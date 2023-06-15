e3b0415028b030a3da6da64636c30217ae1fbfdb
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-15 15:56:27 +0200

    Commit

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -3,3 +3,26 @@
     2. Constrollare che sia presente in una lista
     3. Se sì/Se no
 */
+
+const mailList = [
+    'mail1',
+    'mail2',
+    'mail3',
+    'mail4',
+    'mail5',
+    'mail6',
+    'mail7',
+    'mail8',
+    'mail9',
+    'mail10',
+]
+
+console.log('mailList', mailList, mailList.length, typeof mailList);
+
+
+//credo di dover fare un ciclo per controllare ogni punto del mio array. X sarà il value ottenuto dall'input che devo aggiungere prima della dichiarazione del ciclo
+for (let i = 0; i < mailList.length; i++){
+    if (mailList[i] == x){
+        console.log(i, mailList[i]);
+    }
+}
